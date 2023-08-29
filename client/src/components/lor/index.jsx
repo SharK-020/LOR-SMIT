4b867c9d8f2b427b8d91a4fe4d81c083fc31051f
Samxpi <Samprit62@gmail.com>
2023-08-30 00:12:22 +0530

    modal

--- client/src/components/lor/index.jsx
+++ client/src/components/lor/index.jsx
@@ -87,20 +87,19 @@ const Index = ({
     }
   };
 
-  if (type === "faculty") {
-    content = (
-      <div>
-        <div className="flex justify-between">
-          <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-            {name}
-          </h5>
-          <button
-            onClick={StudentInfo}
-            className="text-white  bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-3 py-2 text-center items-center"
-          >
-            Get Info
-          </button>
-        </div>
+	if (type === "faculty") {
+		content = (
+			<div>
+				<div className="flex justify-between">
+					<h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+						{name}
+					</h5>
+					<button
+						onClick={getInfo}
+						className="text-white  bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+						Get Info
+					</button>
+				</div>
 
         <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
           {status}
@@ -111,42 +110,34 @@ const Index = ({
           {student}
         </p>
 
-        <div className={`flex justify-between ${fshow ? "" : "hidden"}`}>
-          <button
-            onClick={approve}
-            className="text-white  bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
-          >
-            Approve
-          </button>
-          <button
-            onClick={() => setVisible(true)}
-            className="text-white  bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  "
-          >
-            Decline
-          </button>
-        </div>
-        <LorModal
-          closeModal={closeModal}
-          modalIsOpen={modalIsOpen}
-          Student={studentData.student}
-          User={studentData.user}
-        />
-      </div>
-    );
-  } else if (type === "hod") {
-    content = (
-      <div className={` p-5`}>
-        <div className="flex justify-between">
-          <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-            {name}
-          </h5>
-          <button
-            onClick={StudentInfo}
-            className="text-white  bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-3 py-2 text-center items-center"
-          >
-            Get Info
-          </button>
-        </div>
+				<div
+					className={`flex justify-between ${fshow ? "" : "hidden"}`}>
+					<button
+						onClick={approve}
+						className="text-white  bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+						Approve
+					</button>
+					<button
+						onClick={() => setVisible(true)}
+						className="text-white  bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  ">
+						Decline
+					</button>
+				</div>
+			</div>
+		);
+	} else if (type === "hod") {
+		content = (
+			<div className={` p-5`}>
+				<div className="flex justify-between">
+					<h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+						{name}
+					</h5>
+					<button
+						onClick={getInfo}
+						className="text-white  bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+						Get Info
+					</button>
+				</div>
 
         <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
           {status}
@@ -157,36 +148,29 @@ const Index = ({
           {student}
         </p>
 
-        <div className={`flex justify-between ${hshow ? "" : "hidden"}`}>
-          <button
-            onClick={approve}
-            className="text-white  bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
-          >
-            Approve
-          </button>
-          <button
-            onClick={() => setVisible(true)}
-            className="text-white  bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  "
-          >
-            Decline
-          </button>
-        </div>
-        {/* <LorModal
-          closeModal={closeModal}
-          modalIsOpen={modalIsOpen}
-          Student={studentData.student}
-          User={studentData.user}
-        /> */}
-      </div>
-    );
-  } else if (type === "student") {
-    content = (
-      <div className={` p-5`}>
-        <div className="flex justify-between">
-          <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-            {name}
-          </h5>
-        </div>
+				<div
+					className={`flex justify-between ${hshow ? "" : "hidden"}`}>
+					<button
+						onClick={approve}
+						className="text-white  bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+						Approve
+					</button>
+					<button
+						onClick={() => setVisible(true)}
+						className="text-white  bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  ">
+						Decline
+					</button>
+				</div>
+			</div>
+		);
+	} else if (type === "student") {
+		content = (
+			<div className={` p-5`}>
+				<div className="flex justify-between">
+					<h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+						{name}
+					</h5>
+				</div>
 
         <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
           {status}
